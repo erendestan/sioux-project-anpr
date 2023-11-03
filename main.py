4e8d953ef3507cf0a0071358a6c7e84785caa66c
Eren Destan <e.destan@student.fontys.nl>
2023-11-03 10:12:11 +0100

    Comments have been added to the code

--- main.py
+++ main.py
@@ -1,57 +1,85 @@
 import cv2
 import os
 import time
-from read_plate import extract_text_from_image  # Import the text extraction function
+from read_plate import extract_text_from_image  # Import the text extraction function(read_plate.py)
 
+# Path to the Haar Cascade XML file for license plate detection
 haarcascade = "model/haarcascade_russian_plate_number.xml"
 
+# Initialize a video capture object to capture frames from the default camera (camera index 0)
 cap = cv2.VideoCapture(0)
+
+# Set the width and height of the camera feed to 1000 pixels
 cap.set(3, 1000)  # width
 cap.set(4, 1000)  # height
 
+# Minimum area required for a detected object to be considered a license plate
 min_area = 500
+
 count = 0
+
 output_folder = "output_images"
 
+# Check if the output folder exists, and if not, create it
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 license_plate_detected = False  # Flag to track if a license plate has been detected
 last_detection_time = 0  # Time of the last license plate detection
 
+# Start an infinite loop to continuously capture and process frames from the camera feed
 while True:
+    # Capture a frame from the camera
     success, img = cap.read()
 
+    # Create a Haar Cascade classifier for license plate detection
     plate_cascade = cv2.CascadeClassifier(haarcascade)
+
+    # Convert the captured frame to grayscale for better processing
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # Use the detectMultiScale method to identify potential license plates in the grayscale frame
     plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
 
+    # Loop through the detected license plate regions
     for (x, y, w, h) in plates:
         area = w * h
 
+        # Check if the area of the region is greater than min_area and a license plate has not been previously detected
         if area > min_area and not license_plate_detected:
+            # Draw a rectangle around the license plate
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Add text indicating it's a "Number Plate"
             cv2.putText(img, "Number Plate", (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
 
+            # Extract the license plate region
             img_roi = img[y: y + h, x: x + w]
 
-            if time.time() - last_detection_time >= 4:  # Check if 2 seconds have passed
+            # Check if at least 4 seconds have passed since the last detection
+            if time.time() - last_detection_time >= 4:
+                # Save the license plate image to the output folder with a unique filename
                 output_path = os.path.join(output_folder, "plate_img_" + str(count) + ".jpg")
                 cv2.imwrite(output_path, img_roi)
                 count += 1
+
+                 # Update the last detection time
                 last_detection_time = time.time()
 
+                 # Extract text from the license plate using extract_text_from_image method(read_plate.py)
                 extract_text_from_image(output_path, count)
-
+            
             license_plate_detected = True  # Set the flag to True
         else:
             license_plate_detected = False  # Reset the flag to False
 
+    # Display the frame with potential license plate regions marked
     cv2.imshow("Result", img)
 
+    # Check if the 'q' key is pressed to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Release the video capture object and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
--- read_plate.py
+++ read_plate.py
@@ -1,34 +1,44 @@
-import easyocr
+import easyocr #library for text extraction
 import os
 from datetime import datetime
 
 output_directory = "output_text"
 
+# Check if the output directory exists, and if not, create it
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
+# Function to extract text from an image
 def extract_text_from_image(image_path, count):
+    # Initialize the easyocr Reader with the English language
     reader = easyocr.Reader(['en'])
 
+    # Use easyocr to extract text from the given image
     output = reader.readtext(image_path)
 
     # Generate a unique filename based on the current timestamp
     current_time = datetime.now().strftime("%d_%m_%Y_%H_%M")
 
+    # Loop through the extracted text entries
     for entry in output:
         output_text = entry[1]
         confidence_score = entry[2]
 
+        # Skip entries with low confidence scores (less than 0.8)
         if confidence_score < 0.8:
             continue
-
+        
+        # Skip entries that do not have a specific length (e.g., not 6 characters)
         if len(output_text.replace('-', '')) != 6:
             continue
-
+        
+        # Format the extracted text and confidence score
         formatted_line = f"{output_text} (Confidence: {confidence_score})"
 
+        # Define the output text file path
         output_text_file = os.path.join(output_directory, f"plate_{current_time}_{count}.txt")
 
+        # Write the formatted line to the output text file
         with open(output_text_file, "w", encoding="utf-8") as text_file:
             text_file.write(formatted_line)
 
